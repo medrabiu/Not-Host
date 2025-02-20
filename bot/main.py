a24c1642ae12464ffcb516257aa38ce8f5cc3641
ays-tech <54658247+ays-tech@users.noreply.github.com>
2025-02-20 04:18:00 -0800

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import logging
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
